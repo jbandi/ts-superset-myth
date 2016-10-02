bd6e7a3ddcd3d5dc30165fecc8b29dc8459c8865
Jonas Bandi <dev@jonasbandi.net>
2016-10-02 13:09:53 +0200

    Deleting compiler artefacts

--- src/calculate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var map_1 = require('lodash/map');
-// import {map} from 'lodash';
-function calculate(input) {
-    return map_1.default(input, function (n) { return n + 3; });
-}
-exports.calculate = calculate;
-//# sourceMappingURL=calculate.js.map
